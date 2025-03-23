59a1534423ab3f6c9226b4689ea5fadd191b3fb1
= <james.a.padden@gmail.com>
2025-03-23 01:54:38 -0700

    Added relationship between Users and Posts

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -8,9 +8,11 @@ class User(db.Model):
     password = db.Column(db.String(250), nullable=False)
     fname = db.Column(db.String(250), nullable=False)
     lname = db.Column(db.String(250), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
 class Post():
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     title = db.Column(db.String(250), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
