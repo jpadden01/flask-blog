66e57eb02170e771b55f5bbf066301e7c3b400dd
= <james.a.padden@gmail.com>
2025-03-23 01:42:00 -0700

    Created database

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SECRET_KEY"] = "ENTER YOUR SECRET KEY"
+
+db = SQLAlchemy()
 
 from .models import User, Post
 
@@ -8,3 +14,7 @@ from .routes import routes
 from .auth import auth
 app.register_blueprint(routes)
 app.register_blueprint(auth)
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
