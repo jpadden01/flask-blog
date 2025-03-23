ceb1316d7822b6701eabdbfff97e709eb12cee89
= <james.a.padden@gmail.com>
2025-03-23 01:47:32 -0700

    Created User Model

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,12 @@
-class User():
-    pass
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    fname = db.Column(db.String(250), nullable=False)
+    lname = db.Column(db.String(250), nullable=False)
 
 class Post():
     pass
