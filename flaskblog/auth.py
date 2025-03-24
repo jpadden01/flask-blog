96b520d2f008c734843c526810343a6ea958e633
= <james.a.padden@gmail.com>
2025-03-24 01:37:12 -0700

    Created login route which returns 'login.html'

--- flaskblog/auth.py
+++ flaskblog/auth.py
@@ -5,3 +5,7 @@ auth = Blueprint('auth', __name__)
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     return render_template('signup.html')
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
