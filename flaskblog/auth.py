150829421eef1f01c2cd0c32959ce0e1fd8dae27
= <james.a.padden@gmail.com>
2025-03-24 01:00:56 -0700

    Created signup route which returns signup.html

--- flaskblog/auth.py
+++ flaskblog/auth.py
@@ -1,3 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
