6b5ec547383eebb1d121264d3ad93e7a49070b33
= <james.a.padden@gmail.com>
2025-03-24 02:10:48 -0700

    Added verification before adding User

--- flaskblog/auth.py
+++ flaskblog/auth.py
@@ -7,6 +7,15 @@ auth = Blueprint('auth', __name__)
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
+        if User.query.filter_by(username=request.form.get('username')).first():
+            flash('Username taken')
+            return render_template('signup.html')
+        if User.query.filter_by(email=request.form.get('email')).first():
+            flash('Email already in use')
+            return render_template('signup.html')
+        if request.form.get('password') != request.form.get('password-confirm'):
+            flash('Passwords do not match')
+            return render_template('signup.html')
         user = User(
             username=request.form.get('username'),
             email=request.form.get('email'),
@@ -14,6 +23,8 @@ def signup():
             fname=request.form.get('fname'),
             lname=request.form.get('lname')
         )
+        db.session.add(user)
+        db.session.commit()
         flash('Account created!')
     return render_template('signup.html')
 
