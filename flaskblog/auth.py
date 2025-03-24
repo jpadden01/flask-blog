28ab32d70428b6584cbbcdc658ed7df39ce5bccb
= <james.a.padden@gmail.com>
2025-03-24 01:13:22 -0700

    Created unstyled signup form

--- flaskblog/auth.py
+++ flaskblog/auth.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     return render_template('signup.html')
