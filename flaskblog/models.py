1c39c47ad39d4c8c87d9184b024b148c35c32cce
= <james.a.padden@gmail.com>
2025-03-24 02:02:42 -0700

    Fixed error where Post was not a SQLAlchemy Model

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -15,7 +15,7 @@ class User(UserMixin, db.Model):
     lname = db.Column(db.String(250), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
-class Post():
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     title = db.Column(db.String(250), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
