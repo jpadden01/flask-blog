da9922690c99af7208192100e6e1cfe67e54c8a6
= <james.a.padden@gmail.com>
2025-03-24 02:03:23 -0700

    Allowed signup to create an account
    - Form verification not implimented yet

--- flaskblog/auth.py
+++ flaskblog/auth.py
@@ -1,9 +1,20 @@
-from flask import Blueprint, render_template
+from . import db
+from .models import User
+from flask import Blueprint, render_template, flash, request
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        user = User(
+            username=request.form.get('username'),
+            email=request.form.get('email'),
+            password=request.form.get('password'),
+            fname=request.form.get('fname'),
+            lname=request.form.get('lname')
+        )
+        flash('Account created!')
     return render_template('signup.html')
 
 @auth.route('/login')
