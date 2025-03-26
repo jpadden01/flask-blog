d5d1685baf279470eb5397103a0b1f89a5cbad29
= <james.a.padden@gmail.com>
2025-03-26 02:03:35 -0700

    Added flashed message when redirecting from home page to login

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import current_user
 
 routes = Blueprint('routes', __name__)
@@ -6,5 +6,6 @@ routes = Blueprint('routes', __name__)
 @routes.route('/')
 def home():
     if not current_user.is_authenticated:
+        flash('Please log in first')
         return redirect(url_for('auth.login'))
     return render_template('home.html')
